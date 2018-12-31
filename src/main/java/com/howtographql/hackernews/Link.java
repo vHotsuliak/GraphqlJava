e1e06052e02a03d378522f1abd6aac8b52327491
vHotsuliak <vasyl.hotsuliak@gmail.com>
2018-12-31 14:58:08 +0200

    add connectors

--- src/main/java/com/howtographql/hackernews/Link.java
+++ src/main/java/com/howtographql/hackernews/Link.java
@@ -4,10 +4,17 @@ import lombok.Getter;
 
 @Getter
 public class Link {
+
+    private final String id; //the new field
     private final String url;
     private final String description;
 
     public Link(String url, String description) {
+        this(null, url, description);
+    }
+
+    public Link(String id, String url, String description) {
+        this.id = id;
         this.url = url;
         this.description = description;
     }
